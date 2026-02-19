d1330777ba618ea2d6416728c15a8ccb6abace25
Gabriel Winkler <999gabriel.winkler@gmail.com>
2026-02-19 12:33:48 +0100

    security! remove hardocded API Key from code and replace with .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'screens/home_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await initializeDateFormatting('de_DE', null);
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,12 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'favorites_screen.dart';
+
+// api key from .env file
 
 /// Hauptbildschirm der Wetter-App
 class HomeScreen extends StatefulWidget {
@@ -14,8 +17,9 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  // OpenWeatherMap API Key
-  final String apiKey = '711b0df3e461b3c35e8ca67b28920759';
+  // OpenWeatherMap API Key (aus .env Datei geladen)
+  // replace with .env file in production
+  final String apiKey = dotenv.env['OPENMETEOKEY'] ?? '';
 
   // Default location: Innsbruck, Tirol
   double latitude = 47.2692;
